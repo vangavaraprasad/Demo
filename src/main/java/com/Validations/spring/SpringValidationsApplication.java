8afc1dca8da7ee80e857a4d25f26f0fb8781df2b
vangavaraprasad <vangavaraprasad@gmail.com>
2023-02-13 15:32:45 +0530

    changes

--- src/main/java/com/Validations/spring/SpringValidationsApplication.java
+++ src/main/java/com/Validations/spring/SpringValidationsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class SpringValidationsApplication {
 
+	// hello world
 	public static void main(String[] args) {
 		SpringApplication.run(SpringValidationsApplication.class, args);
 	}
